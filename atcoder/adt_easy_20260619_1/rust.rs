ae943be6e6aa7eff23a5fd9c11b8cf330941355c
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2026-06-19 21:32:19 +0900

    Fix) Vector pop()

--- atcoder/adt_easy_20260619_1/rust.rs
+++ atcoder/adt_easy_20260619_1/rust.rs
@@ -5,10 +5,10 @@ fn main() {
     let mut buff= std.lock().lines();
     let inputed: Vec<usize>= buff.next().unwrap().unwrap()
         .split_whitespace().map(|f| f.parse<usize>().unwrap()).collect();
-    let a= inputed[0];
-    let b= inputed[1];
-    let c= inputed[2];
-    let x= inputed[3];
+    let x= inputed.pop();
+    let c= inputed.pop();
+    let b= inputed.pop();
+    let a= inputed.pop();
 
     if a >= x{
         return 1.000000000000;
