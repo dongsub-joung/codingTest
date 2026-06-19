f51c602745ad04d1e03de049bf18c9c454dcd727
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2026-06-19 21:42:56 +0900

    Refector

--- atcoder/adt_easy_20260619_1/rust.rs
+++ atcoder/adt_easy_20260619_1/rust.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let mut answer= 0.000000000000;
+    let mut answer= 0.000000000001;
 
     let std= stdin();
     let mut buff= std.lock().lines();
@@ -11,11 +11,17 @@ fn main() {
     let a= inputed.pop();
 
     if a >= x{
-        return 1.000000000000;
+        answer= 1.000000000000;
     }else{
         let remained= b-a;
-        // @TODO 12geta
-        answer= c % remained;
+
+        // Compare 12 geta vs 12 geta
+        if remained.abs(13) != answer{
+            // @TODO 12geta
+            answer= c % remained;   
+        }else{
+            answer= 0.000000000000;
+        }
     }
 
 
