77686764c819bf2fc6718d28e5f8028f76d14a62
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2026-06-19 20:51:18 +0900

    Add) cpp in brain

---
+++ atcoder/adt_easy_20260619_1/cpp.cpp
@@ -0,0 +1,22 @@
+#define PERFECT 1.000000000000
+
+#include <iostream> 
+
+int main(int argc, char const *argv[])
+{
+	float answer; // 12getamade
+	{
+		int a, int b, int c, int x= std::in >>;
+		if (a <= x){
+			return PERFECT;
+		}else{
+			int remained= b-a;
+			answer= c % remained;
+		}
+	}
+	
+	// #f.12 IDK
+	std::out << {answer} <<;
+
+	return 0;
+}
