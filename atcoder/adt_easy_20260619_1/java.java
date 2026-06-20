0984b72a31f2a0ad3ae6e075758622847503c5ce
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2026-06-21 05:58:34 +0900

    java

--- atcoder/adt_easy_20260619_1/java.java
+++ atcoder/adt_easy_20260619_1/java.java
@@ -1,4 +1,14 @@
-public static void main(String[] args) {
-	Scanner sc= new Scanner(System.in);
+public float q_a(a,b,c,x) {
+
 	
+}
+
+class java{
+	public static void main(String[] args) {
+		int a=30;
+		int b= 500;
+		int c= 20;
+		int x= 103;
+
+	}
 }
