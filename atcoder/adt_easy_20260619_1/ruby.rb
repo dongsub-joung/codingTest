92c43c3443264c26ea64a5e63c730107064295b5
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2026-06-20 11:45:00 +0900

    Fix) logic

--- atcoder/adt_easy_20260619_1/ruby.rb
+++ atcoder/adt_easy_20260619_1/ruby.rb
@@ -1,13 +1,33 @@
-answer=0
-a,b,c,x= gets.chomp
+def rounding_value(ue, shita)
+	puts (@ue / @shita).round(13)
+end
 
-remained= b-a
-answer= c % remained
+
+answer=0
+
+input= gets.chomp
+int_values= input.split().map(&:to_i)
+
+a= int_values[0]
+b= int_values[1]
+c= int_values[2]
+x= int_values[3]
+
+puts a,b,c,x
+
+remained= b - a + 1
+
+answer= rounding_value(c, remained)
+
+if answer.is_a? Integer
+	answer= -1.0
 
 if a >= x
  answer= 1.000000000000
+end
 
-if "#{answer} #f.12 < 0.000000000001
+if answer < 0.000000000001
  answer= 0.000000000000
+end
 
 puts answer
