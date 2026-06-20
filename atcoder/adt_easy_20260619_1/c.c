0ab6de314698381d5bc6ca31ee016f7a79a37cb4
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2026-06-21 05:34:00 +0900

    Add) value remained

---
+++ atcoder/adt_easy_20260619_1/c.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+int main() {
+     int c= 20;
+    int x= 103;
+
+    int remained;
+    {
+        int a= 30; int b= 500;
+        remained= b-a;
+    }
+
+
+    return 0;
+}
