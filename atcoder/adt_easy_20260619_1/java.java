b9b3d4b32ff9e3e841de03a76de446683ce46b3a
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2026-06-21 06:33:53 +0900

    java

--- atcoder/adt_easy_20260619_1/java.java
+++ atcoder/adt_easy_20260619_1/java.java
@@ -1,17 +1,17 @@
-public float q_a(a,b,c,x) {
-
-	
-}
-
 class java{
+	static float q_a(a,b,c,x) {
+
+		
+	}
+
 	public static void main(String[] args) {
 		int a=30;
 		int b= 500;
 		int c= 20;
 		int x= 103;
 
-		float answer= q_a(a,b,c,x);
-		
+		float answer= this.q_a(a,b,c,x);
+
 		System.out.println(answer);
 	}
 }
