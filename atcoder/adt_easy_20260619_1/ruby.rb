122accfa7a98f3bb8dc5fcdf0f2732134bb597ea
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2026-06-20 10:51:29 +0900

    Fix) logic

--- atcoder/adt_easy_20260619_1/ruby.rb
+++ atcoder/adt_easy_20260619_1/ruby.rb
@@ -1,6 +1,13 @@
-a,b,c,x =chome
+answer=0
+a,b,c,x= gets.chomp
+
+remained= b-a
+answer= c % remained
 
 if a >= x
-	
+ answer= 1.000000000000
+
+if "#{answer} #f.12 < 0.000000000001
+ answer= 0.000000000000
 
 puts answer
