fb4150c73deb331332f0bf5bcbb17a303c3be492
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2026-06-20 16:05:20 +0900

    FIx) text case 001

--- atcoder/adt_easy_20260619_1/ruby.rb
+++ atcoder/adt_easy_20260619_1/ruby.rb
@@ -1,27 +1,30 @@
 def rounding_value(ue, shita)
-	puts (@ue / @shita).round(13)
+	rounding_result= ue.to_f / shita.to_f 
+	
+	# puts rounding_result.round(13)
+	return rounding_result.round(13)
 end
 
-
+# init
 answer=0
 
 input= gets.chomp
 int_values= input.split().map(&:to_i)
 
+# puts a,b,c,x
 a= int_values[0]
 b= int_values[1]
 c= int_values[2]
 x= int_values[3]
 
-puts a,b,c,x
-
-remained= b - a + 1
+# puts remained
+remained= b - a
 
 answer= rounding_value(c, remained)
 
 if answer.is_a? Integer
 	answer= -1.0
-
+end
 if a >= x
  answer= 1.000000000000
 end
